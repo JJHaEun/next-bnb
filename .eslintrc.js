9356a78313cf3c7be143a930851a3fef8a238301
JJHaEun <yj05037@gmail.com>
2023-05-16 20:35:24 +0900

    초기세팅 eslint는 airbnb용 사용

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+modue.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: ["airbnb"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: "**/tsconfig.json",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".tsx"] },
+    ],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/indent": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "jsx-quotes": "off",
+    "@typescript-eslint/quotes": "off",
+    "@typescript-eslint/padding-line-between-statements": "off",
+    "arrow-body-style": "off",
+    "capitalized-comments": "off",
+    "@typescript-eslint/consistent-type-imports": "off",
+    "@typescript-eslint/naming-convention": "off",
+  },
+};
---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <div>안녕</div>
+    </>
+  );
+}
